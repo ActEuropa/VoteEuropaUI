1fa13ce81a0dc02b7f35205de4724a55364f2867
karmaecrivain94 <karmaecrivain94@users.noreply.github.com>
2017-02-23 23:05:32 +0100

    Dynamic vote option buttons Animation on ListItem hover Reddit debate URLs implemented

--- VotingUI/js/main.js
+++ VotingUI/js/main.js
@@ -1,10 +1,11 @@
-function QuestionElement(id, title, subtitle, options, HasAnswered, expiredate) {
+function QuestionElement(id, title, subtitle, options, HasAnswered, expiredate, redditurl) {
     this.id = id;
     this.title = title;
     this.subtitle = subtitle;
     this.options = options;
     this.HasAnswered = HasAnswered;
     this.expiredate = expiredate;
+    this.redditurl = redditurl;
 }
 
 function VotingPage_Loaded()
@@ -13,11 +14,13 @@ function VotingPage_Loaded()
 }
 
 //Test questions:
-var qe1 = new QuestionElement(1, "How often should we have discord discussions?", "So, how often?", ["Weekly","Bi-weekly","Monthly","Never"], true, new Date(1490313600000))
+var qe1 = new QuestionElement(1, "How often should we have discord discussions?", "So, how often?", ["Weekly","Bi-weekly","Monthly","Never"], true, new Date(1490313600000),"reddit.com/r/test1")
 var qe2 = new QuestionElement(2, "Is ActEuropa going somewhere?", "Is it?", ["Yes","No","Maybe","Wtf"], true, new Date(1488326400000))
-var qe3 = new QuestionElement(3, "Who should we support in the French elections?", "There's the choice between various people, the main candidates being", ["Emmanuel Macron","François Fillon","Benoît Hamon","JLM"], true, new Date(1491177600000))
-var qe4 = new QuestionElement(4, "Should all muslims be banned?", "🎵 I can show you the world 🎶 (Except the USA)", ["Yes","No","Maybe","Wtf"], false, new Date(1506556800000))
-var Questions = [qe1,qe2,qe3,qe4];
+var qe3 = new QuestionElement(3, "Who should we support in the French elections?", "There's the choice between various people, the main candidates being:", ["Emmanuel Macron","François Fillon","Benoît Hamon","Jean-Luc Mélenchon","Yannick Jadot","Marine Le Pen","Sylvain Duriff"], true, new Date(1491177600000), "reddit.com/r/test1")
+var qe4 = new QuestionElement(4, "Should all muslims be banned?", "🎵 I can show you the world 🎶 (Except the USA)", ["Yes","No"], false, new Date(1506556800000), "reddit.com/r/test1")
+var qe5 = new QuestionElement(5, "Which English food is the worst?", "This is a difficult question.", ["Marmite","Bangers and mash","Cobbler","Black pudding","Devilled kidneys","Jellied eels","Baked beans","Jelly","Pound cake","Spotted dick","Semolina pudding","Liver and onions","All of it"], false, new Date(1503556800000), "reddit.com/r/test1")
+
+var Questions = [qe1,qe2,qe3,qe4, qe5];
 
 function LoadQuestions(page_nb)
 {
@@ -40,21 +43,48 @@ function LoadQuestions(page_nb)
        $(this).siblings().css("cursor","pointer");
        $(this).css("background", "#001f38");
        $(this).css("cursor","default");
+
        for (i = 0; i < Questions.length; i++) 
        {
            if(Questions[i].id == id)
            {
                document.getElementById("qtitle").innerText = Questions[i].title;
                document.getElementById("qsubtitle").innerText = Questions[i].subtitle;
-               //TODO: hide options 3/4 when they are null
-               document.getElementById("qoption1").innerText = Questions[i].options[0];
-               document.getElementById("qoption2").innerText = Questions[i].options[1];
-               document.getElementById("qoption3").innerText = Questions[i].options[2];
-               document.getElementById("qoption4").innerText = Questions[i].options[3];
+               if(Questions[i].redditurl == undefined){ $("#redditbutton").css("visibility","hidden"); }
+               else { $("#redditbutton").css("visibility","visible");}
+               var tbody = document.getElementById("vo_tbody");
+               $("#voteoptions > tbody").html("");
+               var row = tbody.insertRow();
+               var listMode = false;
+               $("#voteoptions").css("border-spacing", "24px")
+               $("#tablewrapper").css("margin", "0px -24px")
+               if(Questions[i].options.length > 4) 
+               {
+                   listMode = true;
+                   $("#voteoptions").css("border-spacing", "4px")
+                   $("#tablewrapper").css("margin", "24px -4px")
+               }
+               for (j = 0; j < Questions[i].options.length; j++) {
+                   if(j % 2 == 0 && j>1 || listMode) row = tbody.insertRow();
+                   var cell = row.insertCell();
+                   cell.setAttribute("class", "VoteButton");
+                   cell.innerText = Questions[i].options[j];
+                   if(listMode) {
+                       cell.style.display = "inline";
+                       cell.style.padding = "0px 4px";
+                   }
+                   else if(Questions[i].options.length == 2){
+                       cell.style.height = "202px";
+                   }
+               }
            }
        }
    });
+   //TODO: fix bug where if you click on an option and move the mouse away faster than the animation, the second function gets fired.
+   $('table.listitem').hover(
+       function() { if($(this).css("background-color") != "rgb(0, 31, 56)") $(this).css("background", "#002443") },
+       function() { if($(this).css("background-color") != "rgb(0, 31, 56)") $(this).css("background", "#00294b") }
+       );
    //TODO: automatically select the most relevant question (the one with the least time left that hasn't yet been answered)
-   //TODO: check background color, and if it isn't #001f38, change it to #002443 (I tried doing this, but it fucked up because of the animation)
-   $('table.listitem').hover(function() {});
+   //TODO: Fire event on .VoteButton click.
 }
